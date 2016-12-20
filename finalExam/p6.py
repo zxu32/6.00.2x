d6f77934ec5087d764b502b3280ca5e9992fe963
zxu32 <zhengxu32@gmail.com>
2016-12-20 01:49:00 -0500

    answered final questions 1~7

--- finalExam/p6.py
+++ finalExam/p6.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def find_combination(choices, total):
     """
     choices: a non-empty list of ints
@@ -13,3 +16,38 @@ def find_combination(choices, total):
     pick the one that gives sum(result*choices) closest
     to total without going over.
     """
+    results = []
+    resultSums = []
+    resXchoi = []
+    powerSetLength = 2**len(choices)
+    npChoices = np.array(choices)
+    # these lists are for cases when there's no match for total
+    resultsClosest = []
+    resXchoiClosest = []
+
+    for i in range(powerSetLength):
+        result = np.append(np.zeros(len(choices) - len(bin(i)[2:]), dtype=np.int64),
+                           list(bin(i)[2:]))
+        result = result.astype(np.int64)
+        rXc = sum(np.multiply(npChoices, result)) - total
+
+        if rXc == 0:  # record results matching total
+            results.append(result)
+            resultSums.append(sum(result))
+            resXchoi.append(rXc)
+
+        if rXc <= 0:  # record results that doesn't go over total
+            resultsClosest.append(result)
+            resXchoiClosest.append(rXc)
+
+    if resXchoi:
+        return results[resultSums.index(min(resultSums))]
+    else:
+        return resultsClosest[resXchoiClosest.index(max(resXchoiClosest))]
+
+print(find_combination(choices=[1,2,2,3], total=4))
+print(find_combination(choices=[1,1,3,5,3], total=5))
+print(find_combination(choices=[4, 6, 3, 5, 2], total=10))
+print(find_combination([1, 3, 4, 2, 5], 16))
+print(find_combination([1], 10))
+print(find_combination([1, 81, 3, 102, 450, 10], 9))
--- test.py
+++ test.py
@@ -1,11 +1,17 @@
-import time
+import numpy as np
+a1 = np.array([1, 2, 3, 4])
+a1.flatten()
+length = 4
+for i in range(2**length):
+    result = np.append(np.zeros(length - len(bin(i)[2:])), list(bin(i)[2:]), axis=0)
+    result2 = result.astype(np.float)
+    # print(type(result), type(a1))
+    # for i in result2:
+    #     print(type(i))
+    print(np.multiply(a1, result2))
 
-# l1 = [1, 2, 3, 4, 1]
+for i in a1:
+    print(type(i))
 
-# print(l1.index(1))
-
-# for i in l1:
-#     for j in l1:
-#         print(l1[i-1:j])
-
-print(bin(10))
+# print(type(a1))
+# print()
